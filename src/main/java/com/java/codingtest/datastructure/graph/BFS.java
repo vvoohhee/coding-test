f5be241a0409c855bc0c0c4cdf6f2713a46ae406
woohee <vvoohhee@gmail.com>
2024-09-09 13:48:39 +0900

    그래프 BFS, DFS 개념 정리, 예제 문제

--- src/main/java/com/java/codingtest/datastructure/graph/BFS.java
+++ src/main/java/com/java/codingtest/datastructure/graph/BFS.java
@@ -2,51 +2,86 @@ package com.java.codingtest.datastructure.graph;
 
 import java.util.*;
 
-/**
- * Graph = G(V,E)
- * 그래프 = 정점(vetex)과 간선(edge)의 집합으로 정점 사이의 연결 관계를 나타내는 자료구조
- * <p>
- * 그래프의 종류
- * 방향그래프 vs 무향그래프 == 정점에 연결된 간선의 방향 유무에 따라 나누는 방식
- * 다중그래프 vs 단순그래프 == 두 정점 사이에 연결된 간선의 개수에 따라 나누는 방식
- * 가중치그래프 (다익스트라) == 간선마다의 가중치가 존재하는 그래프
- * <p>
- * 표현 방법
- * 1) 인접리스트   : vertex:[connectedVertex1, connectedVertex2, .. ] 형태로 인접한 정점 리스트를 HashMap에 저장
- * 2) 인접행렬    : array[i][j] 형태, 각 배열 요소에 i, j 버텍스 간의 간선 개수를 배열로 표현
- * 3) 암시적그래프 : array[i][j] 형태, 직접 간선의 관계를 표현하지 않지만 한 요소를 정점으로 보고 연결됨=1, 간선없음=0으로 표현 (중요)
- * <p>
- * 순회 방법
- * 1) BFS : 기준이 되는 정점에 가까운 순으로 너비 기준 탐색 (가까운 순서대로 탐색 후 그 다음 가까운 순서를 순회)
- * 2) DFS : 기준이 되는 정점에 연결된 정점을 깊이 기준 탐색 (파고파고 끝까지 들어간 뒤 다음 순회)
- */
 public class BFS {
+    static ArrayList<Integer> visitedNodeList = new ArrayList<>();
+    static boolean[][] visitedNodeArray;
+    static int[][] grid;
+
+    // 이동을 위한 경로 : 위, 오른쪽, 아래, 왼쪽 순 (시계방향)
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
+
     public static void main(String[] args) {
-        HashMap<Integer, List<Integer>> adjacencyList = new HashMap<>();
-        adjacencyList.put(0, List.of(1, 3));
-        adjacencyList.put(1, List.of(0, 2));
-        adjacencyList.put(2, List.of(1));
-        adjacencyList.put(3, List.of(0));
-        ArrayList<Integer> visited = bfsWithArrayAdjacencyList(adjacencyList, 0);
+        /*
+         인접리스트인 경우의 예시
+         HashMap<Integer, List<Integer>> adjacencyList = new HashMap<>();
+         adjacencyList.put(0, List.of(1, 3));
+         adjacencyList.put(1, List.of(0, 2));
+         adjacencyList.put(2, List.of(1));
+         adjacencyList.put(3, List.of(0));
+         ArrayList<Integer> visited = bfsWithArrayAdjacencyList(adjacencyList, 0);
+         */
+
+        grid = new int[][]{
+                {1, 1, 1, 1, 0},
+                {1, 1, 0, 1, 0},
+                {1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0}
+        };
+        int row = grid.length;
+        int col = grid[0].length;
+        visitedNodeArray = new boolean[row][col];
+        bfsWithImplicitGraph(0, 0);
     }
 
     /**
      * 인접리스트를 이용한 BFS 구현
+     *
      * @param adjacencyList
      */
-    public static ArrayList<Integer> bfsWithArrayAdjacencyList(Map<Integer, List<Integer>> adjacencyList, int start) {
-        ArrayList<Integer> visited = new ArrayList<>();
-
+    public static void bfsWithArrayAdjacencyList(Map<Integer, List<Integer>> adjacencyList, int start) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(start);
+        visitedNodeList.add(start);
 
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int vertex = queue.poll();
-            for(int edgedVertex : adjacencyList.get(vertex)) {
-                queue.offer(edgedVertex);
-                if(!visited.contains(edgedVertex)) visited.add(edgedVertex);
+            for (int nextVertex : adjacencyList.get(vertex)) {
+                if (!visitedNodeList.contains(nextVertex)) {
+                    queue.offer(nextVertex);
+                    visitedNodeList.add(nextVertex);
+                }
             }
         }
-        return visited;
+    }
+
+    /**
+     * 암시적 그래프가 주어졌을 때 BFS 구현
+     */
+    public static void bfsWithImplicitGraph(int row, int col) {
+        Queue<int[]> queue = new LinkedList<>(); // BFS 구현을 위한 큐 선언 및 초기화
+        queue.offer(new int[]{row, col});
+        visitedNodeArray[row][col] = true;
+
+        while (!queue.isEmpty()) {
+            int[] curNode = queue.poll(); // == {row, col} 형태의 노드의 좌표
+
+            for (int i = 0; i < 4; i++) { // curNode를 기준으로 상하좌우에 있는 모든 노드를 탐색하기 위함
+                int nextRow = curNode[0] + dr[i];
+                int nextCol = curNode[1] + dc[i];
+                if (isValidNode(nextRow, nextCol)) {
+                    if (!visitedNodeArray[nextRow][nextCol]) { // 아직 탐색하지 않은 노드라면 큐와 visited에 추가
+                        queue.offer(new int[]{nextRow, nextCol});
+                        visitedNodeArray[nextRow][nextCol] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean isValidNode(int row, int col) {
+        return row >= 0 && row < grid.length // row의 유효성 확인 : 0 <= (행 좌표) < grid의 세로 길이(=행 크기)
+                && col >= 0 && col < grid[0].length // col의 유효성 확인 : 0 <= (열 좌표) < grid의 가로 길이(=열 크기)
+                && grid[row][col] == 1; // grid[row][col]이 다른 노드와 연결된 노드인지 확인
     }
 }
