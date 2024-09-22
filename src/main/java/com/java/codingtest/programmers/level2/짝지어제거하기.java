b05f2108fd12d99d88a3143026b9b84904710cfb
woohee <vvoohhee@gmail.com>
2024-09-22 20:37:13 +0900

    스택 문제 풀이

---
+++ src/main/java/com/java/codingtest/programmers/level2/짝지어제거하기.java
@@ -0,0 +1,22 @@
+package com.java.codingtest.programmers.level2;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12973
+ */
+public class 짝지어제거하기 {
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+
+            if (!stack.isEmpty() && current == stack.peek()) stack.pop();
+            else stack.push(current);
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
